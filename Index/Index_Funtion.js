2648356004d05b350d2bb43d96ff789bd454e879
Pakelin03 <mcardenas.4094@unimar.edu.ve>
2024-03-09 18:09:19 -0400

    mejora de las responsividad de la lista de personas conectadas

--- Index/Index_Funtion.js
+++ Index/Index_Funtion.js
@@ -35,3 +35,28 @@ $(".file_remove").on("click", function (e) {
     barra_progreso.removeClass("file_uploading");
     barra_progreso.removeClass("file_uploaded");
 });
+
+$(document).ready(function () {
+    // Función para ajustar la altura de la lista
+    function adjustListHeight() {
+        var sidebarHeight = $("#panel-lateral").height();
+        var adjustedHeight = sidebarHeight - 350; // Ajusta este valor según tus necesidades
+
+        // Establecer la altura de la lista
+        $("#chatList").css("max-height", adjustedHeight + "px");
+    }
+
+    // Ajustar la altura de la lista cuando se carga la página
+    adjustListHeight();
+
+    // Ajustar la altura de la lista cuando cambia el tamaño de la ventana
+    $(window).resize(function () {
+        adjustListHeight();
+    });
+
+    // También puedes ajustar la altura de la lista cuando cambie el contenido del sidebar
+    // Por ejemplo, si se agregan o eliminan elementos del sidebar
+    // $("#sidebar").on("change", function() {
+    //     adjustListHeight();
+    // });
+});
