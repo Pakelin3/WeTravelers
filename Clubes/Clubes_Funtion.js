4f7617b51618e8e6f2089231e1ae170331c9681f
Pakelin03 <mcardenas.4094@unimar.edu.ve>
2024-03-09 19:24:51 -0400

    responsividad de lista de amigos añadida a todos los .html

--- Clubes/Clubes_Funtion.js
+++ Clubes/Clubes_Funtion.js
@@ -1,28 +1,3 @@
-async function iniciarMap() {
-    var latitude, longitude;
-
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            latitude = position.coords.latitude;
-            longitude = position.coords.longitude;
-            console.log("Latitud:", latitude);
-            console.log("Longitud:", longitude);
-
-            // Crear el objeto de coordenadas
-            var coord = { lat: latitude, lng: longitude };
-
-            // Crear el mapa
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 10,
-                center: coord
-            });
-        });
-    } else {
-        console.log("Geolocalización no soportada");
-    }
-};
-
-
 $(document).ready(function () {
     // Función para ajustar la altura de la lista
     function adjustListHeight() {
